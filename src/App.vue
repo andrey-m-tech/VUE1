61ccc1f8c1a8eb9778aebe75aa0e1127b941bfd7
ANDREY MININ <andrey.m.tech@gmail.com>
2024-07-20 13:36:25 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyComponent></MyComponent>
+
+  <!-- HTML Input Types
+  https://www.w3schools.com/html/html_form_input_types.asp -->
+
+ <br>
+ Домашнее задание 1 
+ <br>
+ Введите количество товара (1 - 10)
+ <br>
+ {{num}}
+ <button @click="numminus">-</button>
+  <input type='number' v-bind:value="num"    />
+ <button @click="numplus">+</button>
+ <br>
+ <br>
+
+  <label>
+    Введите email:
+    <input type='email' v-bind:value='email' v-on:input="emailInput"/>
+  </label>
+<br>
+  <label>
+    Введите пароль:
+    <input type='password' v-bind:value='password'/>
+  </label>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyComponent from './components/MyComponent.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      email: '123example@mail.ru',
+      password: '12345',
+      num: 1,
+      n: 0
+    }
+  },
+  methods: {
+    
+    numplus() {
+      if (this.num < 10) {
+        this.num += 1
+      }
+        
+    },
+    numminus() {
+      if (this.num > 1) {
+        this.num -= 1
+      }
+    },
+    m() {
+      this.email = this.email + '*'
+    },
+    emailInput(event) {
+      //this.email = event.target.value
+      console.log(event.target.value)
+      this.m()
+    }
+    
+  },
   components: {
-    HelloWorld
+    MyComponent
   }
 }
 </script>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="root"> 
+   class root
+   <br>
+   {{text}}
+  </div>
+
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data() {
+        return {
+            text: 'Привет, Медвед!'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
